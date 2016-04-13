8cd5473512dfcae9e2d8dfb5ddd873a9c393f7f9
Sotiris Lamprinidis <sot.lampr@gmail.com>
2016-04-13 11:50:36 +0300

    Testing pushing to two repos

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 from setuptools import setup
 
-setup(name='FlaskApp',
-      version='1.0',
-      description='A basic Flask app with static files',
-      author='Ryan Jarvinen',
-      author_email='ryanj@redhat.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-     install_requires=['Flask>=0.10.1'],
-     )
+setup(name='Sotlampr Personal Site',
+      version='0.1',
+      description='A basic personal site',
+      author='Sotiris Lamprinidis',
+      author_email='sot.lampr@gmail.com',
+      install_requires=['Flask>=0.10.1'])
